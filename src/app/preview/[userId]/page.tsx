bcc2afee26d3df17fe32ea969535a93d68248dd4
Aminah Rashid <aminahrashid.bsaif20@iba-suk.edu.pk>
2024-07-27 05:11:27 +0500

    added a spinner

--- src/app/preview/[userId]/page.tsx
+++ src/app/preview/[userId]/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import toast, { Toaster } from "react-hot-toast";
 import platforms from "@/utils/logos";
 import { useSession } from "next-auth/react";
+import { Loader2 } from 'lucide-react'
 
 export default function ProfileCard() {
   const router = useRouter();
@@ -20,6 +21,7 @@ export default function ProfileCard() {
   const [profileLinks, setProfileLinks] = useState<
     { platformName: string; url: string }[]
   >([]);
+  const [loading, setLoading] = useState(true);
   const { data: session } = useSession();
 
   useEffect(() => {
@@ -30,9 +32,11 @@ export default function ProfileCard() {
           setUser(data.user);
           setProfileLinks(data.profileLinks.platformLinks);
           console.log(data);
+          setLoading(false);
         })
         .catch((error) => {
           console.error("Error fetching user data:", error);
+          setLoading(false);
         });
     }
   }, [userId]);
@@ -54,7 +58,7 @@ export default function ProfileCard() {
   };
 
   return (
-    <div className="min-h-screen bg-white pb-4">
+    <div className="min-h-[120vh] bg-white pb-4">
       <div className=" sm:bg-primary-default w-full sm:h-[357px] sm:rounded-b-[32px] relative">
         <div className="sm:absolute top-6 left-6 right-6 flex justify-between bg-white p-4 rounded-xl">
           <Button
@@ -73,71 +77,77 @@ export default function ProfileCard() {
             Share Link
           </Button>
         </div>
-        <div className="w-full sm:w-fit relative sm:absolute sm:top-48 sm:left-1/2 sm:transform sm:-translate-x-1/2">
-          <Card className="bg-white p-10 sm:rounded-xl sm:shadow-lg  border-0 sm:border">
-            <div className="flex flex-col items-center">
-              <div className="w-24 h-24 rounded-full overflow-hidden mb-6">
-                <Image
-                  src={user?.profilePic || ""}
-                  alt="Profile"
-                  className="w-24 h-24 rounded-full border-[4px] border-primary-default"
-                  width={200}
-                  height={200}
-                />
-              </div>
-              <h2 className="text-heading-s text-gray-dark mb-1">
-                {user?.firstName} {user?.lastName}
-              </h2>
-              <p className="text-body-m text-gray-default mb-14">
-                {user?.email}
-              </p>
-              <div className="w-full space-y-4">
-                {profileLinks &&
-                  profileLinks.length > 0 &&
-                  profileLinks.map((link, index) => {
-                    const platform = platforms.find(
-                      (p) => p.value === link.platformName
-                    );
-                    if (!platform) return null;
+        {loading ? (
+          <div className="flex items-center justify-center h-full w-full">
+            <Loader2 className="animate-spin text-white" size={24} />
+          </div>
+        ) : (
+          <div className="w-full sm:w-fit relative sm:absolute sm:top-48 sm:left-1/2 sm:transform sm:-translate-x-1/2">
+            <Card className="bg-white p-10 sm:rounded-xl sm:shadow-lg  border-0 sm:border">
+              <div className="flex flex-col items-center">
+                <div className="w-24 h-24 rounded-full overflow-hidden mb-6">
+                  <Image
+                    src={user?.profilePic || ""}
+                    alt="Profile"
+                    className="w-24 h-24 rounded-full border-[4px] border-primary-default"
+                    width={200}
+                    height={200}
+                  />
+                </div>
+                <h2 className="text-heading-s text-gray-dark mb-1">
+                  {user?.firstName} {user?.lastName}
+                </h2>
+                <p className="text-body-m text-gray-default mb-14">
+                  {user?.email}
+                </p>
+                <div className="w-full space-y-4">
+                  {profileLinks &&
+                    profileLinks.length > 0 &&
+                    profileLinks.map((link, index) => {
+                      const platform = platforms.find(
+                        (p) => p.value === link.platformName
+                      );
+                      if (!platform) return null;
 
-                    const isFrontendMentor =
-                      platform.value === "Frontend Mentor";
-                    return (
-                      <Button
-                        key={index}
-                        variant="outline"
-                        className={`w-full justify-between ${
-                          isFrontendMentor ? "text-gray-dark" : "text-white"
-                        } hover:text-primary-default hover:border-primary-default`}
-                        style={{ backgroundColor: platform.color }}
-                        onClick={() => window.open(link.url, "_blank")}
-                      >
-                        {platform.logo}
-                        <span
-                          className={
+                      const isFrontendMentor =
+                        platform.value === "Frontend Mentor";
+                      return (
+                        <Button
+                          key={index}
+                          variant="outline"
+                          className={`w-full justify-between ${
                             isFrontendMentor ? "text-gray-dark" : "text-white"
-                          }
+                          } hover:text-primary-default hover:border-primary-default`}
+                          style={{ backgroundColor: platform.color }}
+                          onClick={() => window.open(link.url, "_blank")}
                         >
-                          {platform.label}
-                        </span>
-                        <svg
-                          className={`w-4 h-4 ${
-                            isFrontendMentor ? "text-gray-dark" : "text-white"
-                          }`}
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                        >
-                          <path d="M9 5l7 7-7 7" />
-                        </svg>
-                      </Button>
-                    );
-                  })}
+                          {platform.logo}
+                          <span
+                            className={
+                              isFrontendMentor ? "text-gray-dark" : "text-white"
+                            }
+                          >
+                            {platform.label}
+                          </span>
+                          <svg
+                            className={`w-4 h-4 ${
+                              isFrontendMentor ? "text-gray-dark" : "text-white"
+                            }`}
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                          >
+                            <path d="M9 5l7 7-7 7" />
+                          </svg>
+                        </Button>
+                      );
+                    })}
+                </div>
               </div>
-            </div>
-          </Card>
-        </div>
+            </Card>
+          </div>
+        )}
       </div>
       <Toaster />
     </div>
